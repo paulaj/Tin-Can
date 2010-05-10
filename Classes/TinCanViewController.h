444330a7d58c603df5614d019e0c5101e2116fb1
Drew Harry <drew.harry@gmail.com>
2010-05-10 12:11:04 -0400

    Initial commit.

---
+++ Classes/TinCanViewController.h
@@ -0,0 +1,16 @@
+//
+//  TinCanViewController.h
+//  TinCan
+//
+//  Created by Drew Harry on 5/10/10.
+//  Copyright MIT Media Lab 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TinCanViewController : UIViewController {
+
+}
+
+@end
+
---
+++ main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TinCan
+//
+//  Created by Drew Harry on 5/10/10.
+//  Copyright MIT Media Lab 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
