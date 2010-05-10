8754b7d55d6f5d09baf0d5dc77999909adde8dc8
Drew Harry <drew.harry@gmail.com>
2010-05-10 19:54:29 -0400

    Added in a stub for the meeting timer view. Nothing there yet, really.

---
+++ ipad/Classes/MeetingTimerView.h
@@ -0,0 +1,18 @@
+//
+//  MeetingTimerView.h
+//  TinCan
+//
+//  Created by Drew Harry on 5/10/10.
+//  Copyright 2010 MIT Media Lab. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface MeetingTimerView : UIView {
+    NSDate *startDate;    
+}
+
+- (id) initWithFrame:(CGRect)frame;
+
+@end
---
+++ ipad/Classes/MeetingTimerView.m
@@ -0,0 +1,21 @@
+//
+//  MeetingTimerView.m
+//  TinCan
+//
+//  Created by Drew Harry on 5/10/10.
+//  Copyright 2010 MIT Media Lab. All rights reserved.
+//
+
+#import "MeetingTimerView.h"
+
+
+@implementation MeetingTimerView
+
+
+- (id) initWithFrame:(CGRect)frame {
+    self = [super initWithFrame:frame];
+    
+    return self;
+}
+
+@end
