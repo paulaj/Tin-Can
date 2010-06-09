2ed6ed9d1d552df21593bfa6d7ac78e7d9fbca39
Drew Harry <drew.harry@gmail.com>
2010-06-09 15:44:19 -0400

    A bunch more progress on types. I think they're all there now, and there's some structure for getting JSON versions of them. This'll make state dumping easier.

--- server/types.py
+++ server/types.py
@@ -16,6 +16,7 @@ Copyright (c) 2010 MIT Media Lab. All rights reserved.
 
 import uuid
 import time
+import simplejson as json
 
 
 class ObjectManager():
@@ -32,28 +33,32 @@ class BaseType(object):
         self.uuid = uuid.uuid4()
         
         # When we have the object store up and running, register this new
-        # object with it. 
-
-class MeetingObjectType(BaseType):
-    """Defines some basic properties that are shared by meeting objects."""
-    def __init__(self, creatorUUID, meetingUUID):
-        BaseType.__init__(self)
+        # object with it.
         
-        # TODO we almost certainly want to unswizzle these UUIDS
-        # to their actual objects. 
-        self.createdBy = creatorUUID
-        self.createdAt = time.time()
-        
-        self.meeting = meetingUUID
+    def getDict(self):
+        return {"uuid":uuid}
+    
+    def getJSON(self):
+        """Return a JSON representation of the type."""
+        return json.dumps(self.getDict())
 
 class Room(BaseType):
-    """Stores the room-related information."""
+    """Store the room-related information."""
+
     def __init__(self, name):
         BaseType.__init__(self)
         self.name = name
         self.currentMeeting = None
+    
+    def getDict(self):
+        d = BaseType.getDict(self)
+        d["name"] = name
+        d["currentMeeting"] = currentMeeting
+        return d
         
 class Meeting(BaseType):
+    """Store meeting-related information."""
+
     def __init__(self, roomUUID, title=None, startedAt=None):
         BaseType.__init__(self)
         self.room = roomUUID
@@ -68,3 +73,84 @@ class Meeting(BaseType):
         self.isLive = False
         self.allParticipants = []
         self.currentParticipants = []
+    
+    def getDict(self):
+        d = BaseType.getDict(self)
+        d["endedAt"] = endedAt
+        d["isLive"] = isLive
+        d["allParticipants"] = allParticipants
+        d["currentParticipants"] = currentParticipants
+        return d
+    
+
+class User(BaseType):
+    """Store meeting-related information."""
+    
+    def __init__(self, name):
+        BaseType.__init__(self)
+        
+        self.name = name
+        self.inMeeting = None
+        self.loggedIn = False
+        self.status = None
+        
+    def getDict(self):
+        d = BaseType.getDict(self)
+        d["name"] = name
+        d["inMeeting"] = inMeeting
+        d["loggedIn"] = loggedIn
+        d["status"] = status
+        return d
+
+
+class MeetingObjectType(BaseType):
+    """Defines some basic properties that are shared by meeting objects."""
+
+    def __init__(self, creatorUUID, meetingUUID):
+        BaseType.__init__(self)
+
+        # TODO we almost certainly want to unswizzle these UUIDS
+        # to their actual objects. When we do that, we'll need to
+        # switch all the getDict methods to getting the UUID instead
+        # of just taking the whole object like it does now.
+        self.createdBy = creatorUUID
+        self.createdAt = time.time()
+
+        self.meeting = meetingUUID
+    
+    def getDict(self):
+        d = BaseType.getDict(self)
+        d["createdBy"] = createdBy
+        d["createdat"] = createdAt
+        d["meeting"] = meeting
+        return d
+
+class Task(MeetingObjectType):
+    """Store information about a task."""
+    
+    def __init__(self, meetingUUID, creatorUUID, text):
+        MeetingObjectType.__init__(self, creatorUUID, meetingUUID)
+        self.text = text
+        self.ownedBy = None
+        
+    def getDict(self):
+        d = MeetingObjectType.getDict(self)
+        d["text"] = text
+        d["ownedBy"] = ownedBy
+
+class Topic(MeetingObjectType):
+    """Store information about a topic."""
+
+    def __init__(self, meetingUUID, creatorUUID, text, timeStarted=None, timeEnded=None):
+        MeetingObjectType.__init__(self, creatorUUID, meetingUUID)
+        self.text = text
+        self.timeStarted = timeStarted
+        self.timeEnded = timeEnded
+
+    def getDict(self):
+        d = MeetingObjectType.getDict(self)
+        d["text"] = text
+        d["timeStarted"] = timeStarted
+        d["timeEnded"] = timeEnded
+        return d
+
